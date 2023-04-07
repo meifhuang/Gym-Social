153aaf19c9379c29659cdfe780dce26537346ee7
meifhuang <meifhuang5@gmail.com>
2023-04-07 17:02:11 -0400

    Add cookies session and register logic using passport local mongoose

--- gym-social/src/main.js
+++ gym-social/src/main.js
@@ -5,7 +5,14 @@ const app = express();
 const cors = require('cors')
 const mongoose = require('mongoose');
 const mongoSanitize = require('express-mongo-sanitize');
-const User = require('./models/User');
+const User = require('./models/user');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const AppError = require('./utils/AppError');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const router = express.Router();
+
 
 mongoose.connect(process.env.MONGO_DB, {
     useNewUrlParser: true,
@@ -25,6 +32,30 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(mongoSanitize());
 
+const sessionConfig = {
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() * 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 ^ 60 * 60 * 24 * 7
+    }
+}
+
+app.use(session(sessionConfig))
+app.use(passport.initialize());
+app.use(passport.session())
+passport.use(new LocalStrategy(User.authenticate())); //comes from passportlocalmongoose
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.body);
+    next();
+})
 
 app.get('/home', (req, res) => {
     res.json({
@@ -38,15 +69,26 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', async (req, res) => {
-    try {
-        const { email } = req.body;
-        const user = new User({ email })
-        await user.save()
-        res.redirect('/home');
-    }
-    catch {
-        console.log(err);
-    }
+    const { email, username, password } = req.body;
+    const user = new User({ email, username })
+    const registeredUser = await User.register(user, password);
+    res.redirect('/home')
+    // req.login(registeredUser, err => {
+    //     if (err) return next(err);
+    //     console.log('success')
+    //     res.redirect('/home')
+    // })
+})
+
+
+app.all('*', (req, res, next) => {
+    next(new AppError('Page Not Found', 404))
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong';
+    res.status(status).send({ err });
 })
 
 
