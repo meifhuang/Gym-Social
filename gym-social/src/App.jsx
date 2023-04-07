6ebfc2f52be07f82dacf3ba4b27be0247daca920
meifhuang <meifhuang5@gmail.com>
2023-04-07 00:47:20 -0400

    Set up simple pages for home and register page

--- gym-social/src/App.jsx
+++ gym-social/src/App.jsx
@@ -2,42 +2,27 @@ import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import axios from 'axios';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./Register";
+import Homepage from "./Homepage";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:3000/hi')
+    axios.get('http://localhost:3000/')
       .then(response => setMessage(response.data.message))
       .catch(error => console.log(error))
   }, []);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div> {message} </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Homepage message={message} />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
