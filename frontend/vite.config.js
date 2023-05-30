7d78ca62569f5be35d59be5c17fc9f2a1c525820
Jacky <jackypengsmail@gmail.com>
2023-05-30 16:58:46 -0400

    Reinstalled package and node modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["styled-components"],
-      globals: { "styled-components": "styled" },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["styled-components"],
+  //     globals: { "styled-components": "styled" },
+  //   },
+  // },
 });
