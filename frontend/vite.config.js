80853cf684daafb6de4639f7816a9e0728cc6686
Jacky <jackypengsmail@gmail.com>
2023-05-30 15:09:10 -0400

    Update again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // build: {
-  //   rollupOptions: {
-  //     external: ["react-is"],
-  //   },
-  // },
-  resolve: {
-    alias: {
-      "react-is": react,
+  build: {
+    rollupOptions: {
+      external: (id) => {
+        // Exclude React and modules starting with 'react-' or 'react/jsx-runtime'
+        return /^react(-.*)?|^react\/jsx-runtime/.test(id);
+      },
     },
   },
 });
