9a8f99333fcc24c3e84f4c824bb1c15ae5f8c7e5
Jacky <jackypengsmail@gmail.com>
2023-05-30 15:39:08 -0400

    Uncommentedd vite-config build rollup react-is

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        "react-is"
-      ]
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: [
+  //       "react-is"
+  //     ]
+  //   },
+  // },
 })
