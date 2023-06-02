ccafe58a7e3703fe982e3fa6e0899b06c2ea2ff8
Jacky <jackypengsmail@gmail.com>
2023-06-01 21:24:57 -0400

    Changed redirect  url route in google auth backend

--- backend/auth/googleauth.js
+++ backend/auth/googleauth.js
@@ -77,7 +77,8 @@ router.get(
       { user: { email: req.user.email }, id: req.user._id },
       process.env.JSONKEY
     );
-    res.redirect(`${FRONTEND_URL}/newsfeed?token=${token}&userId=${req.user._id}`)
+    // res.redirect(`${FRONTEND_URL}/newsfeed?token=${token}&userId=${req.user._id}
+    res.redirect(`/newsfeed?token=${token}&userId=${req.user._id}`)
   }
 );
 
