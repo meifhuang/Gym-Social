368467f6f1d82b2c13738095a3a947bb6698909e
Jacky <jackypengsmail@gmail.com>
2023-09-04 01:27:28 -0400

    Update time stamp

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import TimeAgo from "react-timeago";
-import ReactTimeAgo from "react-time-ago";
-import TimeAgo from "javascript-time-ago";
+// import ReactTimeAgo from "react-time-ago";
+// import TimeAgo from "javascript-time-ago";
 import { MessageTop, MessageAndTimestamp } from "../styledComponents/Chat";
 import en from "javascript-time-ago/locale/en.json";
-TimeAgo.addDefaultLocale(en);
+
+// TimeAgo.addDefaultLocale(en);
+import moment from "moment";
+
+// export const useDate = () => {
+
+//   React.useEffect(() => {
+//       const timer = setInterval(() => { // Creates an interval which will update the current data every minute
+//       // This will trigger a rerender every component that uses the useDate hook.
+//       setDate(new Date());
+//     }, 60 * 1000);
+//     return () => {
+//       clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+//     }
+//   }, []);
+
+//   return moment(new Date(message.createdAt)).fromNow()
+// };
+
 const Message = ({ userMessage, message }) => {
+  // console.log(useDate())
+
+  const [timeAgo, setTimeAgo] = useState(
+    moment(new Date(message.createdAt)).fromNow()
+  );
+  // console.log(timeAgo);
+
+  useEffect(() => {
+    // setTimeAgo(moment(new Date(message.createdAt)).fromNow())
+    const interval = setInterval(() => {
+      setTimeAgo(moment(new Date(message.createdAt)).fromNow());
+    }, 60 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  // console.log(moment(new Date(message.createdAt)).fromNow());
   return (
     <div className={userMessage ? "message user" : "message"}>
       <img src="../images/avatar.png" alt="" />
@@ -14,12 +51,17 @@ const Message = ({ userMessage, message }) => {
           {/* {message.text} */}
           <p className="messageText">{message.text}</p>
         </MessageTop>
-        <ReactTimeAgo
-          date={Date.parse(new Date(message.createdAt))}
+        {/* <ReactTimeAgo
+          date={Date.parse(message.createdAt)}
           locale="en-US"
           timeStyle="round-minute"
           className="timestamp"
-        />
+        /> */}
+        <div>
+          {/* {timeSince(message.createdAt)} */}
+          {/* {moment(new Date(message.createdAt)).fromNow()} */}
+          {timeAgo}
+        </div>
       </MessageAndTimestamp>
     </div>
   );
